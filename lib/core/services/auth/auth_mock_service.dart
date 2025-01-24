23b1347de9498c13a503b2bef150707458c40f09
And5reas <And5reas.porcel@hotmail.com>
2025-01-24 09:35:15 -0300

    Auth mock finish

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,26 +1,63 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
-  ChatUser? get currentUser {
-    return null;
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
   }
 
-  Stream<ChatUser?> get userChanges {}
+  @override
+  ChatUser? get currentUser {
+    return _currentUser;
+  }
 
+  @override
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
+  }
+
+  @override
   Future<void> signup(
-    String nome,
+    String name,
     String email,
     String password,
     File image,
-  ) async {}
+  ) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageUrl: image.path,
+    );
 
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
+  }
+
+  @override
   Future<void> login(
     String email,
     String password,
-  ) async {}
+  ) async {
+    _updateUser(_users[email]);
+  }
 
-  Future<void> logout() async {}
+  @override
+  Future<void> logout() async {
+    _updateUser(null);
+  }
 }
