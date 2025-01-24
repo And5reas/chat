80e48ab006f5000677ba0ea8fdfcc14853fa5832
And5reas <And5reas.porcel@hotmail.com>
2025-01-24 11:07:16 -0300

    showing message

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,5 @@
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +7,32 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mensagens'),
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStream(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(
+            child: Text('Sem mensagens, bó conversar'),
+          );
+        } else {
+          final msgs = snapshot.data!.reversed.toList();
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs.length,
+            itemBuilder: (ctx, index) {
+              final msg = msgs[index];
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(msg.userImageUrl),
+                ),
+                title: Text(msg.text),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
